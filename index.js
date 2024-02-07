42965b4ad0f343dcf063cae49a1684b3d9e1fa69
Atom <Darkxgaming0919@gmail.com>
2024-02-06 23:02:44 -0500

    added some css

--- index.js
+++ index.js
@@ -1,28 +1,55 @@
 import express from "express";
 import bodyParser from "body-parser";
 
-
-
 const app = express();
 const port = 3001;
+
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-let posts=[];
+let posts = [];
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.render("index.ejs");
-})
-app.post("/submit",(req,res)=>{
-    const bodycontent= req.body.status;
+});
+
+app.post("/submit", (req, res) => {
+    const bodycontent = req.body.status;
     posts.push(bodycontent);
-    res.render("posted.ejs", {post:posts})
-}
-)
-app.get("/posts",(req,res)=>{
-    res.render("posted.ejs", {post:posts});
+    res.redirect("/");
+
+});
+app.get("/home", (req,res) => {
+    res.render("index.ejs");
+
 })
 
-app.listen(port,()=>{
-    console.log("Running on port "+ (port))
-})
+app.get("/posts", (req, res) => {
+    res.render("posted.ejs", { post: posts });
+});
+
+app.get("/edit/:index", (req, res) => {
+    const index = req.params.index;
+    res.send(`<form action="/update/${index}" method="post">
+                    <input type="text" name="updatedContent" value="${posts[index]}">
+                    <button type="submit">Update</button>
+                </form>`);
+});
+
+app.post("/update/:index", (req, res) => {
+    const index = req.params.index;
+    const updatedContent = req.body.updatedContent;
+    posts[index] = updatedContent;
+    res.render("posted.ejs", { post: posts });
+});
+
+app.post("/delete/:index", (req, res) => {
+    const index = req.params.index;
+    posts.splice(index, 1);
+    res.render("posted.ejs", { post: posts });
+});
+
+app.listen(port, () => {
+    console.log("Running on port " + port);
+});
